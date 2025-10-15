43f0920adc8cfc5e8e3d5c086d93afd5d00eb6e6
Dan Livings <dan@danlivings.co.uk>
2025-10-15 20:47:48 +0100

    Use parallel copying for performance
    Testing the performance using the Wiremock setup reduces the time
    to copy "Some TV show" from ~70s to ~40s

--- src/fs.rs
+++ src/fs.rs
@@ -1,30 +1,69 @@
 use anyhow::Context;
-use std::path::Path;
+use log::{debug, error};
+use std::io::Error;
+use std::path::{Path, PathBuf};
+use tokio::task::JoinHandle;
 
 pub async fn copy_all(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> anyhow::Result<()> {
-    let metadata = tokio::fs::metadata(&src).await?;
+    let all_paths = get_all_paths(&src).await?;
 
-    if metadata.is_file() {
-        let dir = dst.as_ref().parent().context("Could not get parent dir")?;
-        tokio::fs::create_dir_all(dir).await?;
-        tokio::fs::copy(src, dst).await?;
-    } else {
-        copy_dir(src, dst).await?;
+    let mut handles = Vec::new();
+    for path in all_paths {
+        let src = src.as_ref().to_path_buf();
+        let dst = dst.as_ref().to_path_buf();
+        let handle: JoinHandle<anyhow::Result<()>> = tokio::spawn(async move {
+            let relative_path = path.strip_prefix(&src)?;
+
+            let target_path = if relative_path.as_os_str().is_empty() {
+                dst
+            } else {
+                dst.join(relative_path)
+            };
+
+            let target_path_parent = target_path.parent().context("Could not get parent dir")?;
+            tokio::fs::create_dir_all(target_path_parent).await?;
+
+            debug!("Copying {:?} to {:?}", relative_path, target_path);
+
+            match tokio::fs::copy(path, &target_path).await {
+                Ok(_) => {}
+                Err(err) => {
+                    error!("{}: {}", target_path.display(), err);
+                    Err(err)?
+                }
+            };
+
+            Ok(())
+        });
+        handles.push(handle);
     }
 
-    Ok(())
+    let mut results = Vec::new();
+    for handle in handles {
+        results.push(handle.await?);
+    }
+
+    match results.into_iter().find(|result| result.is_err()) {
+        Some(err) => err,
+        None => Ok(()),
+    }
 }
 
-async fn copy_dir(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> anyhow::Result<()> {
-    tokio::fs::create_dir_all(&dst).await?;
-    let mut s = tokio::fs::read_dir(src).await?;
-    while let Some(entry) = s.next_entry().await? {
-        let file_type = entry.file_type().await?;
-        if file_type.is_dir() {
-            Box::pin(copy_dir(entry.path(), dst.as_ref().join(entry.file_name()))).await?;
-        } else {
-            tokio::fs::copy(entry.path(), dst.as_ref().join(entry.file_name())).await?;
+async fn get_all_paths(path: impl AsRef<Path>) -> anyhow::Result<Vec<PathBuf>> {
+    let metadata = path.as_ref().metadata()?;
+
+    let mut result = Vec::new();
+
+    if metadata.is_file() {
+        result.push(path.as_ref().to_path_buf());
+    } else if metadata.is_dir() {
+        let mut read_dir = tokio::fs::read_dir(path).await?;
+        while let Some(entry) = read_dir.next_entry().await? {
+            let path = entry.path();
+            let mut sub_paths = Box::pin(get_all_paths(path)).await?;
+            result.append(&mut sub_paths);
         }
     }
-    Ok(())
+
+    Ok(result)
 }
